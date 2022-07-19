4dd6c8eed80c2f8ffda1c0a440b07d51a6d365c0
viniciusvalmeida <81371812+viniciusvalmeida@users.noreply.github.com>
2022-07-19 12:06:28 -0300

    Header Bug Correct

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,18 +1,16 @@
 import { Navbar, Container } from "rbx";
 import { useState } from "react";
 import LogoImage from "../../assets/images/logo.png";
-import "../../styles/header.scss"
+import "../../styles/header.scss";
 
 export default function Header() {
     const [openMenu, setOpenMenu] = useState(false);
 
     return (
         <Navbar>
-             
             <Container>
-                   
                 <Navbar.Brand>
-                         <img src={LogoImage} alt="Logo" />     
+                    <img src={LogoImage} alt="Logo" />
                     <Navbar.Burger
                         className="navbar-burger burger"
                         aria-label="menu"
@@ -23,25 +21,17 @@ export default function Header() {
                         <span aria-hidden="true"></span>       
                         <span aria-hidden="true"></span>     
                     </Navbar.Burger>
-                       
                 </Navbar.Brand>
-                   
                 <Navbar.Menu
                     id="navbar-menu"
                     active={openMenu.toString()}
                     onClick={() => setOpenMenu(!openMenu)}
-                >
-                         
-                    <Navbar.Segment
+                ><Navbar.Segment
                         as="div"
                         className="navbar-item navbar-end"
                         align="right"
-                    >
-                              Item 1      
-                    </Navbar.Segment>
-                       
+                    > Item 1</Navbar.Segment>
                 </Navbar.Menu>
-                 
             </Container>
         </Navbar>
     );
