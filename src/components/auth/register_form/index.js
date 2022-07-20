52096f309036564997e6cca66121294ed7f9881b
viniciusvalmeida <81371812+viniciusvalmeida@users.noreply.github.com>
2022-07-20 18:32:55 -0300

    RegisterForm Actions

--- src/components/auth/register_form/index.js
+++ src/components/auth/register_form/index.js
@@ -3,6 +3,15 @@ import { Button, Field, Control, Input, Column, Section, Help, Label } from "rbx
 import { Navigate } from "react-router-dom";
 
 export default function RegisterForm () {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [redirectToLogin, setRedirectToLogin] = useState(false)
+    const [error, setError] = useState(false)
+
+    if (redirectToLogin)
+        return <Navigate to={{pathname: "/login"}}/>
+
     return (
         <>
             <Column.Group centered>
@@ -15,9 +24,11 @@ export default function RegisterForm () {
 
                             <Control>
                                 <Input
-                                    type="name"
                                     required
+                                    type="name"
                                     name="name"
+                                    value={name}
+                                    onChange={e => setName(e.target.value)}
                                 />
                             </Control>
                         </Field>
@@ -29,9 +40,11 @@ export default function RegisterForm () {
 
                             <Control>
                                 <Input
-                                    type="email"
                                     required
+                                    type="email"
                                     name="email"
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
                                 />
                             </Control>
                         </Field>
@@ -43,9 +56,11 @@ export default function RegisterForm () {
 
                             <Control>
                                 <Input
-                                    type="password"
                                     required
+                                    type="password"
                                     name="password"
+                                    value={password}
+                                    onChange={e => setPassword(e.target.value)}
                                 />
                             </Control>
                         </Field>
@@ -54,7 +69,10 @@ export default function RegisterForm () {
                             <Control>
                                 <Column.Group breakpoint="mobile">
                                     <Column>
-                                        <a className="button is-white has-text-custom-purple" href="/login">
+                                        <a 
+                                            className="button is-white has-text-custom-purple"
+                                            onClick={e => setRedirectToLogin(true)}
+                                        >
                                             Login
                                         </a>
                                     </Column>
@@ -70,6 +88,10 @@ export default function RegisterForm () {
                                 </Column.Group>
                             </Control>
                         </Field>
+
+                        { error && <Help color="danger">
+                            Email or Password is invalid
+                        </Help> }
                     </Column>
                 </form>
             </Column.Group>
