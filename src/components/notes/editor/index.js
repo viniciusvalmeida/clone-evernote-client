e33dac4539c46fb1f025bcfdecee82f1a93a5b12
viniciusvalmeida <81371812+viniciusvalmeida@users.noreply.github.com>
2022-07-26 19:07:41 -0300

    RichText

--- src/components/notes/editor/index.js
+++ src/components/notes/editor/index.js
@@ -1,23 +1,35 @@
 import { useEffect, useState } from "react";
 import ReactQuill from "react-quill";
-import 'react-quill/dist/quill.snow.css'
+import "react-quill/dist/quill.snow.css";
 
-export default function Editor(props){
-    const [ currentContent, setCurrentContent ] = useState('')
+export default function Editor (props) {
+    const [currentContent, setCurrentContent] = useState('');
 
     useEffect(() => {
-        setCurrentContent(props.note.body)
-    }, [props.note])
+        setCurrentContent(props.note.body);
+    }, [props.note]);
 
     const modules = {
         toolbar: [
-            [{'header': '1'}, {'header': '2'}, {'font': []},],
-            ['bold', 'italic', 'underline', 'strike', 'blockquote', 'code-black'],
-            [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-            ['link'],
-            ['clean']
-        ]
-    }
+            [{ header: "1" }, { header: "2" }, { font: [] }],
+            [
+                "bold",
+                "italic",
+                "underline",
+                "strike",
+                "blockquote",
+                "code-block",
+            ],
+            [
+                { list: "ordered" },
+                { list: "bullet" },
+                { indent: "-1" },
+                { indent: "+1" },
+            ],
+            ["link"],
+            ["clean"],
+        ],
+    };
 
-    return <ReactQuill value={currentContent} modules={modules}/>
+    return <ReactQuill value={currentContent} modules={modules} />;
 }
