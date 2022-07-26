b4c71e74a74765e51f4e6ed1f54d229043ef0bfb
viniciusvalmeida <81371812+viniciusvalmeida@users.noreply.github.com>
2022-07-26 13:10:41 -0300

    NotesList and Notes API

--- src/components/notes/index.js
+++ src/components/notes/index.js
@@ -1,9 +1,32 @@
 import { useState, useEffect } from "react";
-import { Column, Button } from "rbx";
+import { Column } from "rbx";
 import { push as Menu } from "react-burger-menu";
 import "../../styles/notes.scss";
+import List from "../notes/list";
+import NoteService from "../../services/note"
 
 export default function Notes(props) {
+    const [ notes, setNotes ] = useState([])
+    const [ current_note, setCurrentNote ] = useState({ title: "", body: "", id: "" })
+
+    async function fetchNotes() {
+        const response = await NoteService.index()
+
+        if (response.data.length >= 1) {
+            setNotes(response.data.reverse())
+            setCurrentNote(response.data[0])
+        }
+    }
+
+    useEffect(() => {
+        fetchNotes()
+    }, [])
+
+    const selectNote = (id) => {
+        const note = notes.find((note) => note._id === id)
+        setCurrentNote(note)
+    }
+
     return (
         <>
             <Column.Group
@@ -28,9 +51,11 @@ export default function Notes(props) {
                         </Column>
                     </Column.Group>
 
-                    <p>
-                        List...
-                    </p>
+                    <List
+                        notes={notes}
+                        selectNote={selectNote}
+                        current_note={current_note}
+                    />
                 </Menu>
 
                 <Column
