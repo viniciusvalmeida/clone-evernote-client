908c52c812cc2ec69841329ce4f19b0e937491b0
viniciusvalmeida <81371812+viniciusvalmeida@users.noreply.github.com>
2022-07-29 11:53:03 -0300

    UserEdit

--- src/screens/users/edit/index.js
+++ src/screens/users/edit/index.js
@@ -1,12 +1,10 @@
 import HeaderLogged from "../../../components/header_logged";
-import User from "../../../components/users/"
 
 const UserEdit = () => {
     
     return (
         <>
             <HeaderLogged />
-            <User />
         </>
     )
 }
