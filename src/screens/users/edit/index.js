cf79c25eec0275a600ba0d24c92254aaafcd7bcd
viniciusvalmeida <81371812+viniciusvalmeida@users.noreply.github.com>
2022-07-29 16:09:13 -0300

    UsersEdit Grid

--- src/screens/users/edit/index.js
+++ src/screens/users/edit/index.js
@@ -1,3 +1,4 @@
+import { Card, Column, Container, Section, Title } from "rbx";
 import HeaderLogged from "../../../components/header_logged";
 
 const UserEdit = () => {
@@ -5,6 +6,62 @@ const UserEdit = () => {
     return (
         <>
             <HeaderLogged />
+
+            <Section
+                size="medium"
+                className="users"
+            >
+                <Container>
+                    <Column.Group
+                        centered
+                        className="users-edit"
+                    >
+                        <Column size={4}>
+                            <Title
+                                size={5}
+                                className="has-text-grey has-text-left"
+                            >
+                                Informaões Pessoais
+                            </Title>
+
+                            <Card>
+                                <Card.Content>
+                                    Users Edit Form...
+                                </Card.Content>
+                            </Card>
+                        </Column>
+                    </Column.Group>
+
+                    <Column.Group
+                        centered
+                        className="users-edit"
+                    >
+                        <Column size={4}>
+                            <Title
+                                size={5}
+                                className="has-text-grey has-text-left"
+                            >
+                                Password
+                            </Title>
+
+                            <Card>
+                                <Card.Content>
+                                    Users Edit Password Form...
+                                </Card.Content>
+                            </Card>
+                        </Column>
+                    </Column.Group>
+
+                    <Column.Group centered>
+                        <Column
+                            size={4}
+                            className="has-text-right"
+                        >
+                            Users Delete Button...
+                        </Column>
+                    </Column.Group>
+                </Container>
+            </Section>
         </>
     )
 }
